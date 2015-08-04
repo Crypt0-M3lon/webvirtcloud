64f6a8abe5ab6aec6b177568afa13e2fa736cbe5
Crypt0-M3lon <arthur.villeneuve@gmail.com>
2015-08-04 23:32:47 +0200

    Change group model

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from instances.models import Instance
 
 class Group(models.Model):
     name = models.CharField(max_length=20)
 
     def __unicode__(self):
         return self.name
+
+class Group_instance(models.Model):
+    group = models.ForeignKey(Group)
+    instance = models.ForeignKey(Instance)
